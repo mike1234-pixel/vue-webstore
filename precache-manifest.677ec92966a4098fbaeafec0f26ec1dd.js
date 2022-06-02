75a52b3fa1d46e9f9399f0ba8cce8408e0f8a6c7
Mike <miketandy@protonmail.com>
2022-06-02 19:53:56 +0200

    mt: upd - adding dist subtree

---
+++ precache-manifest.677ec92966a4098fbaeafec0f26ec1dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c28c30aa46d08eec4d74b5ac630af2",
+    "url": "/vue-webstore/assets/acer.jpg"
+  },
+  {
+    "revision": "20c66e216b688240794f23686060b2b2",
+    "url": "/vue-webstore/assets/dell-xps.jpg"
+  },
+  {
+    "revision": "04f34340230830fc0395882faae60467",
+    "url": "/vue-webstore/assets/hp-spectre.jpg"
+  },
+  {
+    "revision": "54551d01511cf5a556cad50345bf4f2c",
+    "url": "/vue-webstore/assets/macbook-air.jpg"
+  },
+  {
+    "revision": "68be66b41f08992522e52e622fc40116",
+    "url": "/vue-webstore/assets/macbook-pro.jpg"
+  },
+  {
+    "revision": "7b5a57b52f19074c2d3b",
+    "url": "/vue-webstore/css/app.08db2788.css"
+  },
+  {
+    "revision": "2ca2344bc17a2bf8b36c7b53515e4ddb",
+    "url": "/vue-webstore/index.html"
+  },
+  {
+    "revision": "7b5a57b52f19074c2d3b",
+    "url": "/vue-webstore/js/app.1d237719.js"
+  },
+  {
+    "revision": "d53f07dcc63a4d1a7da0",
+    "url": "/vue-webstore/js/chunk-vendors.3a6ee1e2.js"
+  },
+  {
+    "revision": "e8ba6a8849b9f6d0e71becddfd374cb4",
+    "url": "/vue-webstore/manifest.json"
+  },
+  {
+    "revision": "53f2d42d7dccbd47d80a5be9ce9e413c",
+    "url": "/vue-webstore/products.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-webstore/robots.txt"
+  }
+]);
